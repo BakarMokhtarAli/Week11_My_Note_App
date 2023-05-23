731ac38e888bf71ed28ad60ce6de0617bd99a7f0
bakar mokhtar <walaalpakar08@gmail.com>
2023-05-23 18:13:55 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 // Soo Jiido wixii components ah ood u baahantahay
 
 import axios from "axios";
+import Notes from "./components/Notes";
+import AddNote from "./components/AddNote";
+import { data } from "autoprefixer";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -10,11 +13,20 @@ function App() {
   useEffect(() => {
     // get all notes from localhost:9000/notes using axios
     // Dhamaan wixii notes ah kasoo jiido localhost:9000/notes adigoo axios isticmaalaayo
+    axios
+      .get("http://localhost:9000/notes")
+      .then((res) => {
+        setNotes(res.data);
+      })
+      .catch((err) => console.error(err));
   }, []);
 
   const createNote = (noteData) => {
-    // Make API call to create a note (POST request to localhost:9000/create_note)
+    // Make API call to create a note (POST request to localhost:9000/)
     // Halkaas ka samee note cusub adigoo POST request isticmaalaayo localhost:9000/create_note
+    axios.post("http://localhost:9000/create_note", noteData).then((note) => {
+      setNotes([note.data, ...notes]);
+    });
   };
 
   const deleteNote = (id) => {
@@ -30,8 +42,10 @@ function App() {
       <div className="w-full">
         <div className="flex flex-col items-center">
           <h3 className="text-3xl text-white mb-5 mt-5">My Notes</h3>
-          { /* Add here all the components you need */ }
-          { /* Halkaas ku dar components-ka aad u baahan tahay */ }
+          {/* Add here all the components you need */}
+          {/* Halkaas ku dar components-ka aad u baahan tahay */}
+          <AddNote createNote={createNote} notes={notes} setNotes={setNotes} />
+          <Notes notes={notes} setNotes={setNotes} />
         </div>
       </div>
     </div>
--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
 
 // Import Formik and Yup
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const AddNote = () => {
- 
+const AddNote = ({ notes, setNotes, createNote }) => {
+  console.log(notes);
   // add form logic here
+  const initialValues = {
+    title: "",
+    content: "",
+  };
+
+  const [noteValue, setNoteValue] = useState(initialValues);
+
+  function handleSubmit(values) {
+    createNote({
+      title: values.title,
+      content: values.content,
+    });
+    setNotes(initialValues);
+  }
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required("The Name Is Required *"),
+    content: Yup.string().required("The Discription Is Required *"),
+  });
 
   return (
-    <div>
-      { /* Add here your form and style it with Tailwind */ }
+    <div className="w-3/5 bg-white shadow-sm py-2 px-6 rounded-lg">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="flex flex-col">
+          <Field
+            type="text"
+            id="title"
+            name="title"
+            placeholder="enter note"
+            className="my-2 px-2 py-2 rounded-sm outline-none border-solid border-solid border-gray border-2 
+            mt-5 rounded-lg rounded-md"
+            autoComplete="off"
+          />
+          <p className="text-red-400 text-sm font-bold -mt-2.5 mb-2 px-0 font-normal">
+            <ErrorMessage name="title" />
+          </p>
+          <Field
+            as="textarea"
+            id="textarea"
+            name="content"
+            className="pt-2 h-40 px-2 pb-5 outline-none border-solid border-solid border-gray border-2 rounded-md"
+          />
+          <p className="text-red-400 font-bld text-sm mx-0 font-normal">
+            <ErrorMessage name="content" />
+          </p>
+          <button
+            type="Submit"
+            className="w-full bg-yellow-400 py-2 rounded-md my-5 font-bold text-center "
+          >
+            Add Note
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
